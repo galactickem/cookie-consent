dce240e49a24f519f759de53dbb3d20001384450
galactickem <a00827581@tec.mx>
2022-11-21 02:31:47 -0600

    Edited Form

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ modalCloseBtn.addEventListener('click', function(){
 
 consentForm.addEventListener('submit', function(e){
     e.preventDefault()
+    const consentFormData = new FormData(consentForm)
+    const fullName = consentFormData.get('fullName')
+
     modalText.innerHTML = `
     <div class="modal-inner-loading">
         <img src="images/loading.svg" class="loading">
@@ -23,9 +26,15 @@ consentForm.addEventListener('submit', function(e){
         document.getElementById('upload-text').innerText = "Making the sale..."
     }, 1500)
 
-    setTimeout(function() {
-        document.getElementById('modal-inner').innerHTML = '<h2>Thanks you sucker! </h2> <p>We just sold the rights to your eternal soul.</p> <div class="idiot-gif"> <img src="images/pirate.gif"> </div>'
-    },3000)
+    setTimeout(function(){
+        document.getElementById('modal-inner').innerHTML = `
+        <h2>Thanks <span class="modal-display-name">${fullName}</span>, you sucker! </h2>
+        <p>We just sold the rights to your eternal soul.</p>
+        <div class="idiot-gif">
+            <img src="images/pirate.gif">
+        </div>
+    `
+    }, 3000)
 
 
   
